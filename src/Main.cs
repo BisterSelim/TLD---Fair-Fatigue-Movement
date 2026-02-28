8aa5c64279203bef7ab1be91f77de887c9a61d74
BisterSelim <107479989+BisterSelim@users.noreply.github.com>
2026-02-28 23:02:45 +0300

    Add files via upload

---
+++ src/Main.cs
@@ -0,0 +1,44 @@
+﻿using HarmonyLib;
+using Il2Cpp;
+using MelonLoader;
+using UnityEngine;
+using ManuelFatigueModu;
+
+[assembly: MelonInfo(typeof(FatigueMain), "Manuel Yorgunluk Modu", "1.0.0", "SeninAdin")]
+[assembly: MelonGame("Hinterland", "TheLongDark")]
+
+namespace ManuelFatigueModu
+{
+    public class FatigueMain : MelonMod
+    {
+        public override void OnInitializeMelon()
+        {
+            Settings.OnLoad();
+            MelonLogger.Msg("Fatigue Control Mod is Ready!");
+        }
+
+        [HarmonyPatch(typeof(Fatigue), "Update")]
+        public class Fatigue_Update_Patch
+        {
+            public static void Postfix(Fatigue __instance)
+            {
+                // Senin bulduğun stabil metodlar
+                bool isWalking = GameManager.GetPlayerManagerComponent().PlayerIsWalking();
+                bool isSprinting = GameManager.GetPlayerManagerComponent().PlayerIsSprinting();
+
+                if (isWalking && !isSprinting)
+                {
+                    // Oyunun varsayılan yürüme maliyetini çekiyoruz
+                    float walkingRate = __instance.m_FatigueIncreasePerHourWalking;
+
+                    // Bu değer üzerinden senin belirlediğin 'indirim' miktarını hesaplıyoruz
+                    // Eğer multiplier 0.2 ise, %80'ini geri iade edeceğiz
+                    float reduction = walkingRate * (1.0f - Settings.options.fatigueMultiplier);
+
+                    // Zaman ölçeği düzeltmesiyle yorgunluğu azaltıyoruz
+                    __instance.AddFatigue(-(reduction * Time.deltaTime / 3600f));
+                }
+            }
+        }
+    }
+}
---
+++ src/Settings.cs
@@ -0,0 +1,28 @@
+﻿using ModSettings;
+
+namespace ManuelFatigueModu // Dikkat: Burası Main.cs ile birebir aynı oldu
+{
+    internal class FatigueModSettings : JsonModSettings
+    {
+        [Name("Yürüme Yorgunluk Çarpanı")]
+        [Description("1.0 varsayılandır. 0.1'e çekerseniz yürüme yorgunluğu %90 azalır. 0.0 yaparsanız yürürken hiç yorulmazsınız.")]
+        [Slider(0f, 1f, 101)]
+        public float fatigueMultiplier = 1.0f;
+
+        [Name("Koşma Yorgunluğuna Etki")]
+        [Description("Eğer açıksa, koşarken (sprinting) harcanan efor da yukarıdaki çarpandan etkilenir.")]
+        public bool affectSprinting = false;
+    }
+
+    internal static class Settings
+    {
+        public static FatigueModSettings options;
+
+        public static void OnLoad()
+        {
+            options = new FatigueModSettings();
+            // Modun oyun içi ayarlar menüsünde görünmesi için bu satır şarttır
+            options.AddToModSettings("Yorgunluk Ayarları");
+        }
+    }
+}
