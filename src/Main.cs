007ec7bb1dfc3a8f1fd231bdaf0a89f7360d3ce8
BisterSelim <107479989+BisterSelim@users.noreply.github.com>
2026-02-28 23:11:22 +0300

    Update Main.cs

--- src/Main.cs
+++ src/Main.cs
@@ -4,7 +4,7 @@ using MelonLoader;
 using UnityEngine;
 using ManuelFatigueModu;
 
-[assembly: MelonInfo(typeof(FatigueMain), "Manuel Yorgunluk Modu", "1.0.0", "SeninAdin")]
+[assembly: MelonInfo(typeof(FatigueMain), "Manuel Fatigue Mod", "1.0.0", "BisterSelim")]
 [assembly: MelonGame("Hinterland", "TheLongDark")]
 
 namespace ManuelFatigueModu
@@ -22,23 +22,17 @@ namespace ManuelFatigueModu
         {
             public static void Postfix(Fatigue __instance)
             {
-                // Senin bulduğun stabil metodlar
                 bool isWalking = GameManager.GetPlayerManagerComponent().PlayerIsWalking();
                 bool isSprinting = GameManager.GetPlayerManagerComponent().PlayerIsSprinting();
 
                 if (isWalking && !isSprinting)
                 {
-                    // Oyunun varsayılan yürüme maliyetini çekiyoruz
                     float walkingRate = __instance.m_FatigueIncreasePerHourWalking;
-
-                    // Bu değer üzerinden senin belirlediğin 'indirim' miktarını hesaplıyoruz
-                    // Eğer multiplier 0.2 ise, %80'ini geri iade edeceğiz
                     float reduction = walkingRate * (1.0f - Settings.options.fatigueMultiplier);
-
-                    // Zaman ölçeği düzeltmesiyle yorgunluğu azaltıyoruz
                     __instance.AddFatigue(-(reduction * Time.deltaTime / 3600f));
                 }
             }
         }
     }
+
 }
